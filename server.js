fb79153735c76dff8b7a746d46a23c43a6d51ead
ramarajanjp <114125913+ramarajanjp@users.noreply.github.com>
2026-04-07 18:29:50 +0530

    Update response message in server.js

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-  res.send("First Project by Arun");
-=======
   res.send("First Project by Ramarajan");
->>>>>>> 542ce4f02a0c63e80db7db5c5672e2d98ebc3751
 });
 
 app.listen(PORT, HOST, () => {
